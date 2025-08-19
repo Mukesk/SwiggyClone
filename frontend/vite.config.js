0c0bf339d6130fc006df10caf36c156e7736f925
Mukeshkanna <94042285+Mukesk@users.noreply.github.com>
2025-08-19 18:11:15 +0530

    Add history API fallback to Vite config
    Integrated 'connect-history-api-fallback' middleware into the Vite development server to support client-side routing. Updated dependencies in package.json and package-lock.json accordingly. Also set server and preview ports to 3000.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import history from "connect-history-api-fallback";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react()],
+  server: {
+    port: 3000,
+    middlewareMode: false,
+    setupMiddlewares(middlewares) {
+      middlewares.push(history());
+      return middlewares;
+    }
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  }
+});
