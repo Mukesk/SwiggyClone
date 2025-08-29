eeee6e1a11b69b917e7242ecc4a18b12775eadb9
Mukeshkanna <94042285+Mukesk@users.noreply.github.com>
2025-08-29 09:39:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 
 app.use(express.json())
 app.use(cors({
-    origin:"https://swiggy-clone-psi-liard.vercel.app/",
+    origin:"https://swiggy-clone-psi-liard.vercel.app",
     methods:"GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders:"Content-Type,Authorization",
     credentials:true
